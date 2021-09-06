c609cb25d0299e95481e12783d03abe99772c42f
Kamran <Kamran02395@mail.ru>
2021-09-06 15:20:28 -0700

    category changed

---
+++ Migrations/20210906012744_parentId.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DentalShop.Migrations
+{
+    public partial class parentId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+
+            migrationBuilder.AddColumn<int>(
+                name: "ParentCategoryId",
+                table: "Categories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_ParentCategoryId",
+                table: "Categories",
+                column: "ParentCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Categories_ParentCategoryId",
+                table: "Categories",
+                column: "ParentCategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Categories_ParentCategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_ParentCategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ParentCategoryId",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<int>(
+                name: "parent_id",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ ViewModels/CategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DentalShop.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int? ParentCategoryId { get; set; }
+
+    }
+}
